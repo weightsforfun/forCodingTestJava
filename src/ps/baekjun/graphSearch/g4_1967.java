34451ffa7412b7385f365ce58ef2a4f8a0f8c5d9
weightsforfun <giyoung914@gmail.com>
2023-09-25 15:54:22 +0900

    Baekjun: g4 dfs 1967 solved

--- src/ps/baekjun/graphSearch/g4_1967.java
+++ src/ps/baekjun/graphSearch/g4_1967.java
@@ -4,17 +4,29 @@ import java.util.*;
 import java.io.*;
 
 public class g4_1967 {
+    public static List<List<Node>> arr=new ArrayList<>();
+    public static int[] visited;
 
+    public static int answer;
+    public static int start;
+    public static class Node{
+        int next;
+        int len;
+        public Node(int next,int len){
+            this.next=next;
+            this.len=len;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int answer=0;
 
         st=new StringTokenizer(br.readLine());
         int n=Integer.parseInt(st.nextToken());
-        int [] distance=new int[n+1];
+        visited=new int[n+1];
 
-        List<List<Integer>> arr=new ArrayList<>();
+
+
         for(int i=0;i<n+1;i++){
             arr.add(new ArrayList<>());
         }
@@ -23,43 +35,37 @@ public class g4_1967 {
             int parent=Integer.parseInt(st.nextToken());
             int child=Integer.parseInt(st.nextToken());
             int value=Integer.parseInt(st.nextToken());
-            distance[child]=value;
-            arr.get(parent).add(child);
+
+            arr.get(parent).add(new Node(child,value));
+            arr.get(child).add(new Node(parent,value));
         }
-//        for(List z:arr){
-//            System.out.println("z = " + z);
-//        }
-        Deque<Integer> deq=new ArrayDeque<>();
-        for(int i=n;i>=0;i--){
-            if(arr.get(i).size()!=0){
-                deq.addLast(i);
+        answer=0;
+        visited[1]=1;
+        dfs(1,0);
+//        System.out.println(answer);
+//        System.out.println(start);
+        answer=0;
+        Arrays.fill(visited,0);
+        visited[start]=1;
+        dfs(start,0);
+        System.out.println(answer);
+    }
+    public static void dfs(int now,int sum){
+
+        List<Node> nexts=arr.get(now);
+//        System.out.println("now "+now+" sum "+sum+" nexts "+nexts.size());
+        if(nexts.size()==1 && sum!=0){
+            if(sum>answer){
+                start=now;
+                answer=sum;
             }
-        }
-
-        while(!deq.isEmpty()){
-
-            int now=deq.pollFirst();
-
-            List<Integer> childs=arr.get(now);
-            if(childs.size()==1){
-                int childIndex=childs.get(0);
-                distance[now]+=distance[childIndex];
-                answer=Math.max(answer,distance[childIndex]);
-            }
-            else if(childs.size()>=2){
-                PriorityQueue<Integer> pq=new PriorityQueue<>((o1,o2)->o2-o1);
-                for(int child:childs){
-                    pq.add(distance[child]);
+        }else{
+            for(Node next:nexts){
+                if(visited[next.next]==0){
+                    visited[next.next]=1;
+                    dfs(next.next,sum+next.len);
                 }
-                int a=pq.poll();
-                int b=pq.poll();
-                answer = Math.max(answer,a+b);
-                distance[now]+=a;
             }
-//            System.out.println("now "+now+" answer = " + answer+" distance now"+distance[now]);
         }
-        System.out.print(answer);
-
-
     }
 }
